bc715856c22fec84eba11b82abc14dcff9eb945b
snehjeetc12 <csnehjeet12@gmail.com>
2021-03-11 19:32:40 +0530

    Refactored (updated maximum method to have generic method)

--- src/main/java/com/testmaximum/TestMaximum.java
+++ src/main/java/com/testmaximum/TestMaximum.java
@@ -1,26 +1,8 @@
 package com.testmaximum;
 
 public class TestMaximum {
-    public static Integer maximum(Integer x, Integer y, Integer z){
-        Integer max = x;
-        if(y.compareTo(max) > 0)
-            max = y;
-        if(z.compareTo(max) > 0)
-            max = z;
-        return max;
-    }
-
-    public static Float maximum(Float x, Float y, Float z){
-        Float max = x;
-        if(y.compareTo(max) > 0)
-            max = y;
-        if(z.compareTo(max) > 0)
-            max = z;
-        return max;
-    }
-
-    public static String maximum(String x, String y, String z){
-        String max = x;
+    public static <T extends Comparable<T>> T maximum(T x, T y, T z){
+        T max = x;
         if(y.compareTo(max) > 0)
             max = y;
         if(z.compareTo(max) > 0)
