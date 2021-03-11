0a9cacf06dbbed7c8fcc48af6a45158016322c89
snehjeetc12 <csnehjeet12@gmail.com>
2021-03-11 19:16:02 +0530

    Maximum method overloaded to find the maximum of three floats

--- src/main/java/com/testmaximum/TestMaximum.java
+++ src/main/java/com/testmaximum/TestMaximum.java
@@ -9,4 +9,13 @@ public class TestMaximum {
             max = z;
         return max;
     }
+
+    public static Float maximum(Float x, Float y, Float z){
+        Float max = x;
+        if(y.compareTo(max) > 0)
+            max = y;
+        if(z.compareTo(max) > 0)
+            max = z;
+        return max;
+    }
 }
--- src/test/java/com/testmaximum/TestMaximumValidator.java
+++ src/test/java/com/testmaximum/TestMaximumValidator.java
@@ -9,20 +9,44 @@ import org.junit.Test;
 public class TestMaximumValidator {
 
     @Test
-    public void givenMaxAtFirst_ShouldReturnTheMaxAtFirst(){
+    public void givenIntegersMaxAtFirst_ShouldReturnTheMaxAtFirst(){
         Integer max = TestMaximum.maximum(40, 30, 20);
         Assert.assertEquals(Integer.valueOf(40), max);
     }
 
     @Test
-    public void givenMaxAtSecond_ShouldReturnTheMaxAtSecond(){
+    public void givenIntegersMaxAtSecond_ShouldReturnTheMaxAtSecond(){
         Integer max = TestMaximum.maximum(30, 40, 20);
         Assert.assertEquals( Integer.valueOf(40), max);
     }
 
     @Test
-    public void givenMaxAtThird_ShouldReturnTheMaxAtThird(){
+    public void givenIntegersMaxAtThird_ShouldReturnTheMaxAtThird(){
         Integer max = TestMaximum.maximum(30, 50, 78);
         Assert.assertEquals(Integer.valueOf(78), max);
+    }
+
+    @Test
+    public void givenFloatsMaxAtFirst_ShouldReturnTheMaxAtFirst(){
+        Float firstFloat = (float)55.23, secondFloat = (float)23.33,
+                thirdFloat = (float)10.92;
+        Float max = TestMaximum.maximum(firstFloat, secondFloat, thirdFloat);
+        Assert.assertEquals(firstFloat, max);
+    }
+
+    @Test
+    public void givenFloatsMaxAtSecond_ShouldReturnTheMaxAtSecond(){
+        Float firstFloat = (float)55.23, secondFloat = (float)93.33,
+                thirdFloat = (float)10.92;
+        Float max = TestMaximum.maximum(firstFloat, secondFloat, thirdFloat);
+        Assert.assertEquals( secondFloat, max);
+    }
+
+    @Test
+    public void givenFloatsMaxAtThird_ShouldReturnTheMaxAtThird(){
+        Float firstFloat = (float)55.23, secondFloat = (float)93.33,
+                thirdFloat = (float)100.92;
+        Float max = TestMaximum.maximum(firstFloat, secondFloat, thirdFloat);
+        Assert.assertEquals(thirdFloat, max);
     }
 }
