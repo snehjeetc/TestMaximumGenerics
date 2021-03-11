368a5a3f338ecbafed82cb22d76045b5f49517f8
snehjeetc12 <csnehjeet12@gmail.com>
2021-03-11 21:00:25 +0530

    Added optional parameters in TestMaximum.maximum method

--- src/main/java/com/testmaximum/TestMaximum.java
+++ src/main/java/com/testmaximum/TestMaximum.java
@@ -1,12 +1,15 @@
 package com.testmaximum;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class TestMaximum {
-    public static <T extends Comparable<T>> T maximum(T x, T y, T z){
-        T max = x;
-        if(y.compareTo(max) > 0)
-            max = y;
-        if(z.compareTo(max) > 0)
-            max = z;
-        return max;
+    public static <T extends Comparable<T>> T maximum(T ...var){
+        if(var == null)
+            return null;
+        List<T> arrayList = Arrays.asList(var);
+        Collections.sort(arrayList, Collections.reverseOrder());
+        return arrayList.get(0);
     }
 }
